201826ec53d2f0fa439f16f7e09906d8050e2a67
Atelier-Mirai <contact@atelier-mirai.net>
2022-04-07 08:40:15 +0900

    一枚のみ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
