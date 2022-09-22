dcf1691a2a7a33eb94cceee1f30b86cc3047fa9c
AtelierMirai <mirai@Mac-Studio.local>
2022-09-22 22:32:52 +0900

    DB seeds.db

--- db/migrate/20220406093338_create_links.rb
+++ db/migrate/20220406093338_create_links.rb
@@ -4,6 +4,9 @@ class CreateLinks < ActiveRecord::Migration[7.0]
       t.string :url
       t.string :text
 
+      t.integer :times_held # 開催回数
+      t.integer :rank       # 写真アルバムの順序
+
       t.timestamps
     end
   end
